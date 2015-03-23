e4bc04009cb5cc049c74f51c25cf7949673fd20c
Thibaut <thibaut.gavard@gmail.com>
2015-03-23 22:59:52 +0100

    Début formulaire d'ajout

--- web/app/services/events.js
+++ web/app/services/events.js
@@ -1,16 +1,28 @@
 angular.module('myApp.services', [])
-.factory('events', ['$http', function($http) {
-    var events = {};
-    events.get = function(begin,end,successCallback) {
-      $http.get('/ws/events/'+begin+'/'+end).
-            success(function(data, status, headers, config) {
-                successCallback(data);
-            }).
-            error(function(data, status, headers, config) {
-                alert(status);
-        });
-      };
+.factory('events', ['$http','$location', function($http,$location) {
+	var events = {};
+	events.get = function(begin,end,successCallback) {
+		$http.get('/ws/events/'+begin+'/'+end).
+			success(function(data, status, headers, config) {
+			successCallback(data);
+		}).
+			error(function(data, status, headers, config) {
+			alert(status);
+		});
+	};
 
-     return events;
-  }
+	events.add = function(e){
+		$http.post('/ws/events', e).
+			success(function(data,status,headers,config){
+			$location.path('/view/app');
+		}).
+			error(function(data,status,headers,config){
+			alert(status);
+		});
+	};
+
+	return events;
+}
 ]);
+
+
--- web/app/view2/view2.js
+++ web/app/view2/view2.js
@@ -3,12 +3,17 @@
 angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
-  $routeProvider.when('/view2', {
-    templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
-  });
+	$routeProvider.when('/view2', {
+		templateUrl: 'view2/view2.html',
+		controller: 'View2Ctrl'
+	});
 }])
 
-.controller('View2Ctrl', [function() {
+.controller('View2Ctrl', ['$scope','events',function($scope, events) {
 
-}]);
+	$scope.addEvent= function(){
+		var event = {"name": $scope.name, "start": $scope.start, "end": $scope.end};
+		events.add(event);
+	}
+}
+]);
