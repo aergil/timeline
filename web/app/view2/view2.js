ffe5e756a3e7c3293c7e4e67791cd24509241d4a
Thibaut <thibaut.gavard@gmail.com>
2015-04-06 01:27:38 +0200

    Gui : Tag autocompletion : sauvegarde

--- web/app/view2/view2.js
+++ web/app/view2/view2.js
@@ -12,12 +12,18 @@ angular.module('myApp.view2', ['ngRoute'])
 .controller('View2Ctrl', ['$scope','events','tags',function($scope, events,tags) {
 		$scope.getEvents = function(val){return events.getByName(val);};
 		$scope.asyncEvent = undefined;
+		$scope.tags = [];
 
 		$scope.create = function(){
 				$scope.asyncEvent = {name:"new",ponctuels:[]};
 		}
 
 		$scope.addEvent= function(){
+				tags = [];
+				angular.forEach($scope.tags, function(value, key) {
+						this.push(value.name);
+				}, tags);
+				$scope.asyncEvent.tags = tags;
 				events.add($scope.asyncEvent);
 		}
 
@@ -33,5 +39,13 @@ angular.module('myApp.view2', ['ngRoute'])
 		$scope.loadTags = function(query){
 				return tags.get(query);
 		}
+
+		$scope.typeaheadOnSelect = function ($item, $model, $label){
+				$scope.tags = [];
+				angular.forEach($item.tags,function(value,key){
+						this.push({"name":value});
+				},$scope.tags)
+		}
+
 }
 ]);
